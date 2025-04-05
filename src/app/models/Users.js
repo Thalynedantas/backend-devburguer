f53358617a416b2d2e3ec115d3398eeaefa0bdac
thalynedantas <thalyne13@gmail.com>
2025-04-05 09:42:59 -0300

    Fazendo login de usuário

--- src/app/models/Users.js
+++ src/app/models/Users.js
@@ -15,13 +15,16 @@ class User extends Model {
                 sequelize,
             },
         );
-        
+
         this.addHook('beforeSave', async (user) => {
             if (user.password) {
                 user.password_hash = await bcrypt.hash(user.password, 10);
             }
             return this;
         });
+    }
+    async checkPassword(password) {
+        return bcrypt.compare(password, this.password_hash);
     }
 }
 
